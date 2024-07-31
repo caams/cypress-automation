45444ae76ed7d870d52f176edabe39795ec1396f
Camila Cruz <ccmiranda@ciencias.unam.mx>
2024-07-31 16:58:15 -0600

    Started Cypress Automation Project.

---
+++ cypress/e2e/firstTest.cy.js
@@ -0,0 +1,14 @@
+describe('Launch Application', () => {
+    it('Launch the test application!', () => {
+        cy.visit('https://www.saucedemo.com/')
+    })
+})
+
+describe('Login scenario', () => {
+    it('Test 1 - Valid Login', () => {
+
+    })
+    it('Test 2 - Invalid Login', () => {
+
+    })
+})
